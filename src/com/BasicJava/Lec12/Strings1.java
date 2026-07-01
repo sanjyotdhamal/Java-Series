bc04105c7a8c086c53348497099f33dfd35ce4f5
Sanjyot Dhamal <sanjyotdhamal31@gmail.com>
2026-07-01 23:06:47 +0530

    lect 12

---
+++ src/com/BasicJava/Lec12/Strings1.java
@@ -0,0 +1,11 @@
+//WAP to take input from user and print it on screen
+import java.util.*;
+
+public class Strings1 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name:- ");
+        String name = sc .next();;
+        System.out.print("your name is :- " + name);
+    }  
+}
---
+++ src/com/BasicJava/Lec12/Strings2.java
@@ -0,0 +1,11 @@
+//WAP to concatenate two strings and print it on screen
+import java.util.*;
+
+public class Strings2 {
+    public static void main (String[] args) {
+        String firstName= "sanjyot";
+        String lastname="Dhamal";
+        String fullname = firstName + " " + lastname;
+       System.out.print("your full name is :- " + fullname);
+    }  
+}
